e7401a4fbd2871c091b593d188589f613dda9d6d
Dasun-Sandaru <105912727+Dasun-Sandaru@users.noreply.github.com>
2025-01-03 09:30:21 +0530

    go const

---
+++ veriables/const.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var userAge int = 25
+	fmt.Println(userAge)
+
+	// assign a new value to the variable
+	userAge = 26
+	fmt.Println(userAge)
+
+
+	// constant
+	// calculate the area of a circle
+	const pi = 3.14
+	var radius float32 = 10
+	var area = pi * radius * radius
+	fmt.Println(area)
+
+
+}
